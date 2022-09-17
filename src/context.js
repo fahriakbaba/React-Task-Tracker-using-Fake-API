e929def3d9a8819ef328bb980ca619d53bcef2f5
Fahri Akbaba <fahriakbaba@gmail.com>
2022-09-17 17:00:28 +0300

    to set up JSON server

--- src/context.js
+++ src/context.js
@@ -3,44 +3,7 @@ import React from "react";
 const AppContext = React.createContext();
 
 function AppProvider( {children} ) {
-    const [items, setItems] = React.useState([
-        {
-          text: "Clean House",
-          day: "Saturday",
-          reminder: true,
-          id: 5,
-        },
-        {
-          text: "Eat a fish",
-          day: "Monday",
-          reminder: false,
-          id: 6,
-        },
-        {
-          text: "Shopping meal",
-          day: "Monday",
-          reminder: false,
-          id: 7,
-        },
-        {
-          text: "Wash a car",
-          day: "Tuesday",
-          reminder: true,
-          id: 8,
-        },
-        {
-          text: "Brush your teeth",
-          day: "Everyday",
-          reminder: false,
-          id: 9,
-        },
-        {
-          text: "Go to the cinema ",
-          day: "one day per a week",
-          reminder: false,
-          id: 10,
-        },
-      ]);
+    const [items, setItems] = React.useState([]);
 
       console.log("setItems: ", setItems);
 
@@ -54,3 +17,44 @@ function AppProvider( {children} ) {
 
 const useGlobalContext = () => React.useContext(AppContext);
 export {useGlobalContext, AppProvider};
+
+/*
+[
+  {
+    "text": "Clean House",
+    "day": "Saturday",
+    "reminder": true, 
+    "id": 5
+  },
+  {
+    "text": "Eat a fish",
+    "day": "Monday",
+    "reminder": false,
+    "id": 6
+  },
+  {
+    "text": "Shopping meal",
+    "day": "Monday",
+    "reminder": false,
+    "id": 7
+  },
+  {
+    "text": "Wash a car",
+    "day": "Tuesday",
+    "reminder": true,
+    "id": 8
+  },
+  {
+    "text": "Brush your teeth",
+    "day": "Everyday",
+    "reminder": false,
+    "id": 9
+  },
+  {
+    "text": "Go to the cinema ",
+    "day": "one day per a week",
+    "reminder": false,
+    "id": 10
+  }
+]
+*/
