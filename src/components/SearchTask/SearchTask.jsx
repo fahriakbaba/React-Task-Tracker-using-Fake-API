182d2351f70fa75e317b3b7f154775e9ec40ea35
Fahri Akbaba <fahriakbaba@gmail.com>
2022-09-17 22:22:09 +0300

    SearchTask is styled by using module css

--- src/components/SearchTask/SearchTask.jsx
+++ src/components/SearchTask/SearchTask.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import styles from "./SearchTask.module.css";
 
-function SearchTask( {handleSearch, search} ) {
+function SearchTask({ handleSearch, search }) {
   return (
-    <section>
-        <input type="text" placeholder='Please, search tasks  you want.' value={search} onChange={handleSearch} />
+    <section className={styles.container}>
+      <h3 className={styles.title}>Search Task</h3>
+      <input
+        type="text"
+        placeholder="Please, search tasks  you want."
+        value={search}
+        onChange={handleSearch}
+        className={styles.input}
+      />
     </section>
-  )
+  );
 }
 
 export default SearchTask;
